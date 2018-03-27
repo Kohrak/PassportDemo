ea3f8b147ef6b4b46994b78fb8c247b43dd48f88
Kohrak <gabrielgt95@gmail.com>
2018-03-27 21:14:25 +0100

    check is user is logged in

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get("/", function(req, res){
   res.render("home");
 })
 
-app.get("/secret", function(req,res){
+app.get("/secret", isLoggedIn, function(req,res){
   res.render("secret");
 })
 
@@ -63,6 +63,13 @@ app.get("/logout", function(req, res){
   req.logout();
   res.redirect("/");
 });
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 //Cloud9
 
