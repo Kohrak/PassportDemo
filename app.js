86a88935d2a63c08e7473e4c5ecace32729239d6
Kohrak <gabrielgt95@gmail.com>
2018-03-27 21:05:18 +0100

    add login and register functionality

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ var express = require("express"),
     mongoose = require("mongoose"),
     passport = require("passport"),
     bodyParser = require("body-parser"),
-    localStrategy = require("passport-local"),
+    LocalStrategy = require("passport-local"),
     passportLocalMongoose = require("passport-local-mongoose"),
     User = require("./models/user");
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
 var app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
   secret: "I like turtles",
   resave: false,
   saveUninitialized: false
 }));
 
-app.set("view engine", "ejs");
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -29,6 +32,32 @@ app.get("/secret", function(req,res){
   res.render("secret");
 })
 
+//Auth routes
+app.get("/register", function(req, res){
+  res.render("register");
+})
+
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/secret");
+        });
+    });
+});
+//login routes
+app.get("/login", function(req, res){
+  res.render("login");
+})
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
 //Cloud9
 
 // app.listen(process.env.PORT, process.env.IP, function(){
